6018301710f39bb8e9704b3c0b664a66ac65aa45
Tofuhippo <washingtonj@carleton.edu>
2018-12-29 16:39:20 -0600

    file movement

--- api/api.js
+++ api/api.js
@@ -4,18 +4,18 @@ var base_url = 'http://mobfup.wpdevcloud.com';
 
     getToken() {
         var data = fetch( base_url + "/wp-json/jwt-auth/v1/token",
-        	  	{method: 'POST',
-        	   	 headers: {
-        	   		'Content-Type': 'application/json'
-        	   	 	},
-        	   	 body: json.stringify({
-        	   		username: config.username,
-        	   		password: config.password
-        	   		})
-        		}
-        	  )
+                {method: 'POST',
+                 headers: {
+                    'Content-Type': 'application/json'
+                    },
+                 body: json.stringify({
+                    username: config.username,
+                    password: config.password
+                    })
+                }
+              )
         .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
-  		.catch(error => console.error(error));
+        .catch(error => console.error(error));
     }
     
     getAllContacts() {
