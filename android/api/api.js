7df411cd1a9263979d614ed3541ab73bd586c957
Tofuhippo <washingtonj@carleton.edu>
2018-12-29 16:37:02 -0600

    hotfix getToken)

--- android/api/api.js
+++ android/api/api.js
@@ -1,11 +1,18 @@
 class DiscipleAPI {
 
 var base_url = 'http://mobfup.wpdevcloud.com';
+
     getToken() {
         var data = fetch( base_url + "/wp-json/jwt-auth/v1/token",
-        	  {username: '',
-        	   password: '',
-        	   token: ''}
+        	  	{method: 'POST',
+        	   	 headers: {
+        	   		'Content-Type': 'application/json'
+        	   	 	},
+        	   	 body: json.stringify({
+        	   		username: config.username,
+        	   		password: config.password
+        	   		})
+        		}
         	  )
         .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
   		.catch(error => console.error(error));
