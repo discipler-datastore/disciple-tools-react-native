62b5e256a0c8b16fae262ec15a34355f38fffbb7
Tofuhippo <washingtonj@carleton.edu>
2018-12-29 16:30:16 -0600

    getToken updated, baseurl added

--- android/api/api.js
+++ android/api/api.js
@@ -1,7 +1,14 @@
 class DiscipleAPI {
 
+var base_url = 'http://mobfup.wpdevcloud.com';
     getToken() {
-        
+        var data = fetch( base_url + "/wp-json/jwt-auth/v1/token",
+        	  {username: '',
+        	   password: '',
+        	   token: ''}
+        	  )
+        .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+  		.catch(error => console.error(error));
     }
     
     getAllContacts() {
