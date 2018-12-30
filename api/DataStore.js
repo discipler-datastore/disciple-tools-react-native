d5123558713b684fa8b596f56c77a29bf4fb3bf9
Eric Gan <eric@ercgn.com>
2018-12-30 15:53:31 -0600

    pushed dummy data

--- api/DataStore.js
+++ api/DataStore.js
@@ -1,53 +1,72 @@
-import fetch from 'react-native';
+import React from 'react';
 
 export default class DataStore {
     static base_url = 'http://mobfup.wpdevcloud.com';
 
     static async getTokenAsync(config) {
-        const response = await fetch(DataStore.base_url + "/wp-json/jwt-auth/v1/token", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username: config.username,
-                password: config.password
-            })
-        });
+        let response;
+        try {
+            response = await fetch(DataStore.base_url + "/wp-json/jwt-auth/v1/token", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: config.username,
+                    password: config.password
+                })
+            });
+        } catch (error) {
+            console.log(error);
+            return;
+        }
 
         const dataJson = JSON.parse(response);
-        return dataJson["token"];
+        const token = dataJson["token"];
+
+        console.log(token);
+        return token;
     }
-    
+
     static async getAllContactsAsync(authToken) {
-        const response = await fetch(DataStore.base_url + "/wp-json/dt/v1/contacts", {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + authToken
-            }
-        });
+        return [
+            new Contact("Mohammad", "Active", "Contact Attempt Needed", [], "John", [], []),
+            new Contact("Reza", "Waiting to be accepted", "Contact Established", [], "Mary", [], []),
+            new Contact("Ali", "Waiting to be accepted", "First Meeting Scheduled", [], "Joseph", [], []),
+            new Contact("Ahmed", "Waiting to be accepted", "First Meeting Scheduled", [], "David", [], []),
+            new Contact("Zya", "Active", "Contact Established", [], "Daniel", [], []),
+            new Contact("Mohammad", "Paused", "Contact Attempt Needed", [], "Alex", [], []),
+            new Contact("Mohanad", "Closed", "Contact Attempt Needed", [], "Jon", [], []),
+        ];
 
-        const dataJson = JSON.parse(response);
-        const contacts = [];
+        // const response = await fetch(DataStore.base_url + "/wp-json/dt/v1/contacts", {
+        //     method: 'GET',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Authorization': 'Bearer ' + authToken
+        //     }
+        // });
 
-        contactsArray = dataJson["contacts"];
-        if (!contactsArray.isArray())
-            throw new Error("Server returned a non-array for list of all contacts.");
+        // const dataJson = JSON.parse(response);
+        // const contacts = [];
 
-        contactsArray.forEach(contactJson => {
-            contacts.push(new Contact(
-                contactJson["post_title"], // name
-                contactJson["overall_status"], // status
-                contactJson["seeker_path"], 
-                contactJson["milestones"], // name
-                contactJson["assigned_to"]["name"], // assignedTo
-                contactJson["locations"], // locations
-                contactJson["groups"] // name
-            ));
-        });
+        // contactsArray = dataJson["contacts"];
+        // if (!contactsArray.isArray())
+        //     throw new Error("Server returned a non-array for list of all contacts.");
 
-        return contactsArray;
+        // contactsArray.forEach(contactJson => {
+        //     contacts.push(new Contact(
+        //         contactJson["post_title"], // name
+        //         contactJson["overall_status"], // status
+        //         contactJson["seeker_path"],
+        //         contactJson["milestones"], // name
+        //         contactJson["assigned_to"]["name"], // assignedTo
+        //         contactJson["locations"], // locations
+        //         contactJson["groups"] // name
+        //     ));
+        // });
+
+        // return contactsArray;
     }
 
     static async getContactByIdAsync(authToken, contactId) {
